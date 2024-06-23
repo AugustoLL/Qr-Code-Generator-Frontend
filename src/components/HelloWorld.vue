d0bcb7d49571c32a8444f36385dc1ae189c5cc68
Augusto L <juanaugustolombino@protonmail.ch>
2024-06-23 17:44:32 -0300

    Test api from frontend

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <v-container class="fill-height">
     <v-responsive
       class="align-centerfill-height mx-auto"
-      max-width="900"
+      max-width="1200"
     >
 
       <div class="text-center">
@@ -12,7 +12,7 @@
       <div class="py-4"></div>
 
       <v-row>
-        <v-col cols="6">
+        <v-col :cols="mobile ? 12 : 6">
           <v-card
             class="py-4"
             color="secondary"
@@ -21,32 +21,33 @@
           >
           <v-card-title>Let's make a QR Code!</v-card-title>
           <v-card-subtitle>Complete the following form:</v-card-subtitle>
-            <v-form>
+            <v-form v-model="isFormValid" @submit.prevent="generateQRCode">
               <v-container>
                 <v-row>
+                  <!-- <input type="url" name="url" id="url" v-model="url.value"> -->
                   <v-text-field
                     class="mb-2 mx-4"
-                    :v-model="URL.value"
                     prepend-icon="mdi-link-variant"
                     label="URL"
-                    :rules="URL.rules"
+                    v-model="url.value.value"
+                    :rules="url.rules"
                     hide-details="auto"
                     type="url"
                   ></v-text-field>
                 </v-row>
-                <v-card-title>Format</v-card-title>
                 <v-row>
+                  <v-icon icon="mdi-image-multiple"></v-icon>
+                  <v-card-title>Format</v-card-title>
                   <v-chip-group
-                    :v-model="FORMAT.value"
                     class="mb-2 mx-4"
-                    prepend-icon="mdi-image"
                     label="Format"
-                    :rules="FORMAT.rules"
+                    v-model="format.value.value"
+                    :rules="format.rules"
                     hide-details="auto"
                     mandatory
                   >
                   <v-chip 
-                    v-for="format in FORMATS"
+                    v-for="format in validFormats"
                     :key="format"
                     :text="format"
                     :value="format"
@@ -55,12 +56,57 @@
                   ></v-chip>
                 </v-chip-group>
                 </v-row>
+                <v-row>
+                  <v-text-field
+                    class="mb-2 mx-4"
+                    prepend-icon="mdi-ruler"
+                    label="Size"
+                    v-model="size.value.value"
+                    :rules="size.rules"
+                    hide-details="auto"
+                    type="number"
+                  ></v-text-field>
+                </v-row>
+                <v-row>
+                  <v-color-picker 
+                  class="mb-2 mx-4"
+                    v-model="patternColor.value.value" 
+                    mode="hex" 
+                    :modes="['hex']"
+                    rounded="lg"
+                    :width="mobile ? '100%' : '40%'"
+                  ></v-color-picker>
+                  <v-color-picker 
+                    class="mb-2 mx-4"
+                    v-model="backgroundColor.value.value" 
+                    mode="hex" 
+                    :modes="['hex']"
+                    rounded="lg"
+                    :width="mobile ? '100%' : '40%'"
+                  ></v-color-picker>
+                </v-row>
+                <v-row>
+                  <v-text-field
+                    class="mb-2 mx-4"
+                    prepend-icon="mdi-image-filter-tilt-shift"
+                    label="Logo URL"
+                    v-model="logoUrl.value.value"
+                    :rules="logoUrl.rules"
+                    hide-details="auto"
+                    type="url"
+                  ></v-text-field>
+                </v-row>
+                <v-row>
+                  <v-btn type="submit" :disabled="!isFormValid" variant="outlined" color="primary" class="mt-4">
+                    Generate
+                  </v-btn>
+                </v-row>
               </v-container>
             </v-form>
           </v-card>
         </v-col>
 
-        <v-col cols="6">
+        <v-col :cols="mobile ? 12 : 6">
           <v-card
             class="py-4 px-4"
             color="primary"
@@ -68,14 +114,23 @@
             variant="outlined"
           >
             <v-img
+            v-if="qrCodeImage"
+            :src="qrCodeImage"
+            alt="QR Code"
             rounded="lg"
             position="center"
-            src="https://github.com/AugustoLL/Qr-Code-Generator-Backend/raw/main/examples/youtubeQRCode.png"
-            ></v-img>
+            >
+              <template v-slot:placeholder>
+                <div class="d-flex align-center justify-center fill-height">
+                  <v-progress-circular
+                    color="primary"
+                    indeterminate
+                  ></v-progress-circular>
+                </div>
+              </template>
+            </v-img>
 
-            <v-btn variant="outlined" color="primary" class="mt-4">
-              Generate
-            </v-btn>
+            <h2>{{ error }}</h2>
 
             <template #title>
               <h2 class="text-h5 font-weight-bold">Preview</h2>
@@ -83,16 +138,8 @@
 
             <template #subtitle>
               <div class="text-subtitle-1">
-                Change this page by updating <v-kbd>{{ `<HelloWorld />` }}</v-kbd>.
               </div>
             </template>
-            <v-overlay
-              opacity=".06"
-              scrim="primary"
-              contained
-              model-value
-              persistent
-            />
           </v-card>
         </v-col>
       </v-row>
@@ -101,23 +148,102 @@
 </template>
 
 <script setup lang="ts">
-  const URL = {
-    value: '',
+  import { ref, watch, computed } from 'vue';
+  import { useDisplay } from 'vuetify';
+  import axios from 'axios';
+
+  const { mobile } = useDisplay();
+
+  let error = '';
+  const qrCodeImage = ref('');
+  const isFormValid = ref(false);
+  const url = {
+    value: ref(''),
+    formatted: computed(() : string => {
+      if (!url.value.value.startsWith('http://') && !url.value.value.startsWith('https://')) {
+        return `https://${url.value.value}`;
+      }
+      return url.value.value;
+    }),
     rules: [
+      (value: string) => typeof value === 'string' || 'Must be a string',
       (value: string) => !!value || 'This field is required.',
       (value: string) => value.length <= 2000 || 'Max 2000 characters',
       (value: string) => {
         const pattern = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
-        return pattern.test(value) || 'Invalid URL.'
+        return pattern.test(value) || 'Invalid URL. Example: https://example.com'
       },
     ],
   };
-  const FORMAT = {
-    value: 'png',
+  const validFormats = ['png', 'jpg', 'jpeg'];
+  const format = {
+    value: ref('png'),
     rules: [
       (value: string) => !!value || 'This field is required.',
       (value: string) => ('png' || 'jpg' || 'jpeg') === value || 'Invalid format.',
     ],
   };
-  const FORMATS = ['png', 'jpg', 'jpeg'];
+  const size = {
+    value: ref(500),
+    rules: [
+      // (value: number) => typeof value === 'number' || 'Must be a number.',
+      (value: number) => !!value || 'This field is required.',
+      (value: number) => value >= 100 || 'Min 100px',
+      (value: number) => value <= 1080 || 'Max 1080',
+    ],
+  };
+  const patternColor = {
+    value: ref('#000000'),
+    rules: [
+      (value: string) => !!value || 'This field is required.',
+    ],
+  };
+  const backgroundColor = {
+    value: ref('#ffffff'),
+    rules: [
+      (value: string) => !!value || 'This field is required.',
+    ],
+  };
+  const logoUrl = {
+    value: ref(''),
+    rules: [
+      (value: string) => value.startsWith('http://') || value.startsWith('https://') || 'Invalid URL.',
+      (value: string) => value.length <= 2000 || 'Max 2000 characters',
+      (value: string) => {
+        const pattern = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
+        return pattern.test(value) || 'Invalid URL. Example: https://example.com'
+      },
+    ],
+  };
+  const generateQRCode = async () => {
+    const params = new URLSearchParams({
+      url: url.formatted.value,
+      format: format.value.value,
+      size: size.value.value.toString(),
+      darkColor: patternColor.value.value,
+      lightColor: backgroundColor.value.value,
+      logoUrl: logoUrl.value.value,
+    });
+
+    try {
+      const response = await axios.get(`http://localhost:8080/api/generate?${params}`, { responseType: 'arraybuffer' });
+
+      const base64String = btoa(
+        new Uint8Array( await response.data).reduce(
+          (data, byte) => data + String.fromCharCode(byte),
+          ''
+        )
+      );
+
+      qrCodeImage.value = `data:image/${format.value.value};base64,${base64String}`;
+    } catch (error) {
+      console.error(error);
+      qrCodeImage.value = '';
+      error = 'An error occurred while generating the QR code.'
+    }
+  };
+
+  watch([url.value, format.value, size.value, qrCodeImage.value], () => { 
+    if (isFormValid.value) generateQRCode();
+  });
 </script>
